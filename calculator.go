599df6eb409f23ddcad7a04b1ffe05cd761cc86a
zdk <di.warachet@gmail.com>
2023-09-04 19:29:33 +0700

    refactor: make a few changes for more readability

--- calculator.go
+++ calculator.go
@@ -1,29 +1,29 @@
 package main
 
+const memberDiscount = 0.9
+
+var setPrices = map[string]float64{
+	"Red":    50,
+	"Green":  40,
+	"Blue":   30,
+	"Yellow": 50,
+	"Pink":   80,
+	"Purple": 90,
+	"Orange": 120,
+}
+
 type Calculator struct{}
 
 func (c *Calculator) CalculatePrice(order map[string]int, hasMemberCard bool) float64 {
-
-	// Define set_prices for each set
-	set_prices := map[string]float64{
-		"Red":    50,
-		"Green":  40,
-		"Blue":   30,
-		"Yellow": 50,
-		"Pink":   80,
-		"Purple": 90,
-		"Orange": 120,
-	}
-
 	// Calculate the total price
 	total := 0.0
-	for color, price := range set_prices {
+	for color, price := range setPrices {
 		total += price * float64(order[color])
 	}
 
 	// Apply member card discount
 	if hasMemberCard {
-		total *= 0.9
+		total *= memberDiscount
 	}
 
 	return total
