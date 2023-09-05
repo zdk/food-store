66192d16647204e20f8cec0288af1d854a22266b
zdk <di.warachet@gmail.com>
2023-09-05 11:26:50 +0700

    refactor: remove fmt and add more test cases

--- calculator_test.go
+++ calculator_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -15,18 +14,53 @@ func TestCalculatePrice(t *testing.T) {
 		expected      float64
 	}{
 		{
-			name: "Test single item without discount",
+			name: "Single item without discount",
 			orderItems: OrderItems{
 				"Red": 1,
 			},
 			hasMemberCard: false,
 			expected:      50.0,
 		},
+		{
+			name: "Single item with quantity > 1 and discount",
+			orderItems: OrderItems{
+				"Green": 2,
+			},
+			hasMemberCard: false,
+			expected:      76.0, // 40 * 2 * 0.95
+		},
+		{
+			name: "Multiple items, some with discount",
+			orderItems: OrderItems{
+				"Red":    2,
+				"Green":  3,
+				"Purple": 1,
+			},
+			hasMemberCard: false,
+			expected:      304.0, // (50*2)+((40*3)*0.95)+90
+		},
+		{
+			name: "Multiple items with member card discount",
+			orderItems: OrderItems{
+				"Red":    2,
+				"Green":  3,
+				"Purple": 1,
+			},
+			hasMemberCard: true,
+			expected:      273.6, // 304.0 * 0.9
+		},
+		{
+			name: "Item not in itemsMap",
+			orderItems: OrderItems{
+				"NonExistentColor": 2,
+			},
+			hasMemberCard: false,
+			expected:      0.0, // Not found in itemsMap, hence price is 0
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fmt.Println(tt.name, tt.orderItems)
 			got := calc.CalculatePrice(tt.orderItems, tt.hasMemberCard)
 			if got != tt.expected {
 				t.Errorf("Expected %f, but got %f", tt.expected, got)
